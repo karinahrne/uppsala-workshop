c7d8f82453f532725a1d9d64b6ec98f65d4d2b88
Karin Ahrné <karin.ahrne@slu.se>
2018-10-11 15:13:28 +0200

    added automate_run.bash, script in bash

---
+++ automate_run.bash
@@ -0,0 +1,16 @@
+##Adjust your path on your machine
+#input directory for molecules data
+INPUT_DIR=/c/Users/ahrne/data-shell/molecules
+#output directory where we will save results
+RESULT_DIR=/c/Users/ahrne/data-shell/result #$PWD/results
+#Do not change here except if you know what you are doing!
+
+rm -rf $RESULT_DIR
+mkdir $RESULT_DIR
+touch $RESULT_DIR/lengths.txt
+for filename in $INPUT_DIR/*.pdb
+   do
+      wc -l $filename | sort -n | head -n 1 >> $RESULT_DIR/lengths.txt
+done
+cat $RESULT_DIR/lengths.txt
+
